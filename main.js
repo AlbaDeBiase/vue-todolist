10b73abf5368be2a3dd050c5e0c2e0e417c1d17c
Alba De biase <debiasealba@gmail.com>
2020-11-11 15:16:40 +0100

    push in array

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // definire una lista di todo e stamparli in pagina in un elenco
-// predisporre un input testuale per aggiungere un nuovo todo: al click di un pulsante, il testo inserito nell'input viene aggiunto alla lista dei todo
+// predisporre un input testuale per aggiungere un nuovo todo: al click di un pulsante,
+// il testo inserito nell'input viene aggiunto alla lista dei todo
 // al click su un item della lista, il todo viene rimosso
 // BONUS: intercettare anche il tasto "ENTER" per aggiungere un nuovo todo
 
 var app = new Vue(
     {
         el: '#container',
-        data: {
+        data() {
+        return{
             titolo: 'Lista di cose da fare',
             azioni: [
                         { message: 'studiare'},
@@ -14,7 +16,20 @@ var app = new Vue(
                         { message:'ripassare'},
                         { message:'comprare il latte'},
                         { message:'andare a correre'}
-                    ]
+                    ],
+            nuova_azione: '',
 
-    }
+        }
+},
+// mounted: function() {
+// this.inserisci_item();
+// },
+        methods: {
+            inserisci_item(nuova_azione){
+            console.log(this.nuova_azione);
+        // azioni.push(this.nuova_azione);
+},
+}
+
+
 })

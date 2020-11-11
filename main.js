073cfd8ce3e358f2328a3aa0a7c73b48ecd269aa
Alba De biase <debiasealba@gmail.com>
2020-11-11 11:51:40 +0100

    creato lista

---
+++ main.js
@@ -0,0 +1,20 @@
+// definire una lista di todo e stamparli in pagina in un elenco
+// predisporre un input testuale per aggiungere un nuovo todo: al click di un pulsante, il testo inserito nell'input viene aggiunto alla lista dei todo
+// al click su un item della lista, il todo viene rimosso
+// BONUS: intercettare anche il tasto "ENTER" per aggiungere un nuovo todo
+
+var app = new Vue(
+    {
+        el: '#container',
+        data: {
+            titolo: 'Lista di cose da fare',
+            azioni: [
+                        { message: 'studiare'},
+                        { message:'leggere'},
+                        { message:'ripassare'},
+                        { message:'comprare il latte'},
+                        { message:'andare a correre'}
+                    ]
+
+    }
+})
